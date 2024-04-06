e78120454b37f2d37f3bf470611c51699cdb8ae5
Aleksandra Wojcik <olawujcik@wp.pl>
2024-04-06 10:11:58 +0200

    nowy push

---
+++ first.java
@@ -0,0 +1,14 @@
+public class example {
+    public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    }
+    }
+trgerhgrehgterg
+ethhbere
+regregregbehberherh
+wdwdrw3
+
+ghgfhfgh
+
+ghfdghd gdf ggdg 
+
