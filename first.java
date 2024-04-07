8eb95a3dbd24b46b43babeee42f59186674e107d
Aleksandra Wojcik <olawujcik@wp.pl>
2024-04-07 09:38:37 +0200

    add new file

--- first.java
+++ first.java
@@ -1,15 +1,23 @@
-public class example {
+import java.util.Scanner;
+
+public class program {
     public static void main(String[] args) {
-    System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj liczbę przedmiotów: ");
+        int ileprzedmiotow = scanner.nextInt();
+        double[] tablicaocen= new double[ileprzedmiotow];
+        for (int i = 0; i < ileprzedmiotow; i++) {
+            System.out.print("Podaj ocenę z przedmiotu " + (i + 1) + ": ");
+            tablicaocen[i] = scanner.nextDouble();
+        }
+        
+        double suma = 0;
+        
+        for (double ocena : tablicaocen){
+          suma += ocena;
+        }
+        double srednia = suma / ileprzedmiotow;
+        System.out.println("Średnia ocen: " + srednia);
+        scanner.close();
     }
-    }
-trgerhgrehgterg
-ethhbere
-regregregbehberherh
-wdwdrw3
-
-ghgfhfgh
-
-ghfdghd gdf ggdg 
-
-hello
+}
